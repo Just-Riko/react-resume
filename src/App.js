72c9bbb6d60979daf35d96cf7000b700de856bc4
Just-Riko <kitchakpavlonew@gmail.com>
2022-03-10 15:57:46 +0200

    hashRouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <br></br>
       <br></br>
       <ul>
-        <li><NavLink to='/react-resume'>About me</NavLink></li>
+        <li><NavLink to='/AboutUs'>About me</NavLink></li>
         <li><NavLink to='/skills'>skills</NavLink></li>
         <li><NavLink to='/projects'>projects</NavLink></li>
         <li><NavLink to='/contacts'>contacts</NavLink></li>
@@ -21,7 +21,7 @@ function App() {
 
         <Routes>
           <Route path='/*' element={<div>Error</div>} />
-          <Route path='/react-resume' element={<div>About me</div>} />
+          <Route path='/AboutUs' element={<div>About me</div>} />
           <Route path='/skills' element={<div>skills</div>} />
           <Route path='/projects' element={<div>projects</div>} />
           <Route path='/contacts' element={<div>contacts</div>} />
